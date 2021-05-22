919d27594021cb9f2ba07373c88e0f6332fd176d
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-22 20:22:48 +0000

    Done.

--- test.rb
+++ test.rb
@@ -1,24 +1,90 @@
+#class definition
 class Dog 
+  attr_accessor :name, :hair, :color
+  attr_reader :gender   #gender reading from instance vaiable from gender
+  attr_writer: :last_meal #last_meal=
   
+  # attr_accessor :name is equivalent to code below. It adds to two instant methods a reader and writer. 
+  
+  # def name=(name)
+  #   @name = name
+  # end
+  
+  # def name=(name)
+  #   @name
+  # end
+  
+    def bark
+      puts "Woof!!!"
+    end
 end
 
-class Baby #class Definition
-  def initialize #Hook/Callback - because it is a method automtically fired cry
+class Cat 
+  attr_reader :name
+  
+  def initialize(name)  #hook, callback or lifecycle 
+    @name = name
+    @born_on = Time.now 
+  end
+  
+  def age 
+    Time.now - @born_on
+  end
+
+end
+
+$ALL_BABIES = []
+
+class Baby
+  attr_accessor :name 
+  def initialize(name = nil)
     cry
-  end
+    @name = name 
+  end 
   
-  def cry #Instance method - represents an object ability to have logic
-    puts "Waaaaa"
-  end
-    
-  def name=(the_baby_name) #the writer
-    @my_name = the_baby_name #Casting the local variable to an instance variable. / hoisting. 
-  end
-  
-  def name #reader 
-    @my_name
+  #save the baby. Put it somewhere so it won't get link_to_unless_current
+     $ALL_BABIES << ?
+  def cry 
+    puts "WAAAAA!!!!!!!"
   end
 end
 
 
-north_west = Baby.new #Instantiation - which means bringing an object ot life.
+
+#1. Make them cry when they are born 
+#2. Give them a name (reader and writer)
+#3. Make them cry through method cry
+#4. Allow names's to be optionally set when they are born
+
+# north_west = Baby.new  #=> "WAAAAA!!!!!!!"
+# north_west.name  = "North West" 
+
+
+# blue_ivy = Baby.new  #=> "WAAAAA!!!!!!!"
+# blue_ivy.name  = "Blue Ivy" 
+
+# blue_ivy.cry  #=> "WAAAAA!!!!!!!"
+# north_west.cry  #=> "WAAAAA!!!!!!!"
+
+
+
+
+
+
+
+class Baby 
+  att
+  
+  
+  
+  
+  
+end
+
+
+foo.bark #=> "Woof!!!"
+
+foo.name =  "FooDog"
+foo.name #=> "FooDog"
+  
+  
